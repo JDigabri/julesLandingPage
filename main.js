bc1702820f5e78a599618282909d3fe6396d652e
JDigabri <johndigabriele@gmail.com>
2023-05-17 12:15:02 -0400

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+
+`
+
+setupCounter(document.querySelector('#counter'))
