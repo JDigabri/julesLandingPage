dd425439fcabad59c51fa5184597bf66f4a0a16b
JDigabri <johndigabriele@gmail.com>
2023-05-17 15:43:33 -0400

    #6

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
+//document.querySelector('#app').innerHTML = ``
 
-    <h1> hello </h1>
-
-`
-
-setupCounter(document.querySelector('#counter'))
+//setupCounter(document.querySelector('#counter'))
